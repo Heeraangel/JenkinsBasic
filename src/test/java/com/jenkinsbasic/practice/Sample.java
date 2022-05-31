8b91526eb4af0bae9a9c417524875ffd09c3ba7a
HP <HP@HEERA>
2022-05-31 13:51:16 +0530

    updated

--- src/test/java/com/jenkinsbasic/practice/Sample.java
+++ src/test/java/com/jenkinsbasic/practice/Sample.java
@@ -2,6 +2,7 @@ package com.jenkinsbasic.practice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
@@ -9,13 +10,15 @@ public class Sample {
 	@Test
 	public void parameterTest()
 	{
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Users\\HP\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
 		WebDriver driver = null;
 		String url="http://localhost:8888";
 		String browser="chrome";
 		switch(browser)
 		{
 			case "chrome":
-				driver=new ChromeDriver();
+				driver=new ChromeDriver(options);
 				break;
 			case "edge":
 				driver=new EdgeDriver();
